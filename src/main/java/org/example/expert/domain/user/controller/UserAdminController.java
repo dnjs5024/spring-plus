c89036202884a2873b27027e4509b3268b127912
dnjs5024 <dnjs7469@naver.com>
2025-05-07 12:22:56 +0900

    Fix : LV1-5 AOP 동작 수정 changeUserRole() 실행 전 동작

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.aop.AdminAccessLoggingAspect;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
 import org.springframework.web.bind.annotation.PatchMapping;
